6b74e9d15dabd3c581fb69a4ae5fb0be3cdb2140
prothegee <prothegee@proton.me>
2026-03-30 21:51:49 +0700

    re-examine of pointer global, with some note

--- pointer_global.zig
+++ pointer_global.zig
@@ -1,25 +1,37 @@
 const std = @import("std");
 
-const Data = struct {
-    x: i32,
-};
+const g = @import("a_global_data.zig");
 
-var g_pData: ?*Data = null;
-var gpa = std.heap.page_allocator;
+pub fn main() !u8 {
+    g.pData = try g.newData();
+    defer _ = g.pData;
 
-pub fn main() !void {
-    g_pData = try gpa.create(Data);
-    // defer _ = g_pData;
+    const nums = [_]i32 {1,2,3,4,5};
 
-    std.debug.print("g_pData created\n", .{});
-
-    if (g_pData) |p| {
-        std.debug.print("p.x #1: {d}\n", .{p.x});
-        p.x = 69;
-        std.debug.print("p.x #2: {d}\n", .{p.x});
+    // std.debug.print("g_pData created\n", .{});
+    if (g.pData == null) {
+        std.debug.print("Error: g.pData still null", .{});
+        return 1;
     }
 
-    // while(true){}
+    if (g.pData) |v| {
+        std.debug.print("v: {any}\n", .{v.num});
+
+        for (nums) |n| {
+            v.num = n;
+            std.debug.print("v: {any}\n", .{v.num});
+        }
+
+        v.num = 6;
+    }
+
+    if (g.pData) |v| {
+        std.debug.print("v: {any}\n", .{v.num});
+    }
+
+    // So how do I modified that data pointer outside/without the statement?
 
     std.debug.print("finished!\n", .{});
+
+    return 0;
 }
